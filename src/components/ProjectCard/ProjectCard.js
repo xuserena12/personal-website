dd5bb20119ac389bfc26c7236d5ee76bbedcc9d9
Serena X <84420380+xuserena12@users.noreply.github.com>
2024-09-18 10:59:52 -0400

    Fix image resizing

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -6,8 +6,8 @@ export default function ProjectCard({ project }) {
     return (
         <div className="project-wrapper">
             <div className="project-card-container">
-                <a href={project.source} target="_blank" rel="noopener noreferrer">
-                    <img src={project.imageSrc} alt={project.title} className="project-image" />
+                <a href={project.source} target="_blank" rel="noopener noreferrer" className="project-link">
+                    <img src={project.imageSrc} alt={project.title} />
                 </a>
             </div>
             <div className="info">
